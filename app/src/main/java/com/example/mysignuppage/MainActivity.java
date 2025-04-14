ddc4bba2862daaaf84d3a7c3cdd32bb7fed7a849
TejaNareddy-git <tejntkr@gmail.com>
2025-04-14 23:24:41 +0530

    first commit

---
+++ app/src/main/java/com/example/mysignuppage/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mysignuppage;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        EditText username = findViewById(R.id.username);
+        MaterialButton regbtn = findViewById(R.id.signupbtn);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username1 = username.getText().toString();
+                Toast.makeText(MainActivity.this, "Username is " + username1, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
